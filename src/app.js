dcd41cbade90b591ce3b8f5f1aea6c3f325fb1a0
leechanyoung <yab0403@naver.com>
2024-12-19 23:49:51 +0900

    cors check

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import postListRouter from './routes/postListRouter.js';
 import userRouter from './routes/userRouter.js';
 import commentRouter from './routes/commentRouter.js';
 import morgan from 'morgan';
+import cors from 'cors';
 
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
