b38d122311dca14d0b61ab719213ff22d885d394
leechanyoung <yab0403@naver.com>
2024-12-27 19:25:03 +0900

    s3 v3 upd

--- src/middlewares/imageMiddleware.js
+++ src/middlewares/imageMiddleware.js
@@ -1,35 +1,35 @@
-import AWS from 'aws-sdk';
-import dotenv from 'dotenv';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 import path from 'path';
-
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-AWS.config.update({
+const s3 = new S3Client({
   region: 'ap-northeast-2',
-  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  },
 });
 
-const s3 = new AWS.S3();
-
-const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp']
+const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp'];
 
 const imageUploader = multer({
   storage: multerS3({
     s3: s3,
     bucket: 'my-first-image-buck',
+    acl: 'public-read-write',
     key: (req, file, callback) => {
-      const uploadDirectory = req.query.directory ?? ''
+      const uploadDirectory = req.query.directory ?? '';
       const extension = path.extname(file.originalname);
       if (!allowedExtensions.includes(extension)) {
-        return callback(new Error('Wrong extension'))
+        return callback(new Error('Wrong extension'));
       }
+      callback(null, `${uploadDirectory}/${Date.now()}-${file.originalname}`);
     },
-    acl: 'public-read-write'
-  })
+  }),
 });
 
-export default imageUploader
+export default imageUploader;
